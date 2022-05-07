7dcf6f97f7cbe3fbd5682a7011430cc68c895910
cathy <cathy@bubulazi.com>
2022-05-07 18:41:41 +0100

    Navegacion terminada falta accecibilidad

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ navToggle.addEventListener("click", () => {
     const visiblity = nav.getAttribute("data-visible");
     if (visiblity === "false") {
         nav.setAttribute("data-visible", true);
+        navToggle.setAttribute("aria-expanded", true)
     } else {
         nav.setAttribute("data-visible", false);
+        navToggle.setAttribute("aria-expanded", false)
     }
 })

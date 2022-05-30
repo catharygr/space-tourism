0c30a2dde18398e324b45086324d34f58eb53ca6
cathy <cathy@bubulazi.com>
2022-05-30 14:44:18 +0100

    Proyecto terminado

---
+++ README.md
@@ -0,0 +1,6 @@
+# Projecto curso HTML, CSS y JS en Scrimba.
+
+Por Colin Powell.
+Diseño Fronend Mentor
+
+Proyecto hecho cconjutamente con @drkedrkedjon
--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const navegacionPrincipal = document.querySelector('.navegacion-principal')
 const movileNavToggle = document.querySelector('.mobile-nav-toggle')
 
 movileNavToggle.addEventListener('click', () => {
-   const visibilidad = navegacionPrincipal.getAttribute('data-visible') 
+  const visibilidad = navegacionPrincipal.getAttribute('data-visible')
 
-   if(visibilidad === 'false') {
-     navegacionPrincipal.setAttribute('data-visible',true)
-     movileNavToggle.setAttribute('aria-expanded',true)
-   } else {
-    navegacionPrincipal.setAttribute('data-visible',false)
-    movileNavToggle.setAttribute('aria-expanded',false)
-   }
-} )
+  if (visibilidad === 'false') {
+    navegacionPrincipal.setAttribute('data-visible', true)
+    movileNavToggle.setAttribute('aria-expanded', true)
+  } else {
+    navegacionPrincipal.setAttribute('data-visible', false)
+    movileNavToggle.setAttribute('aria-expanded', false)
+  }
+})
 
 
 
@@ -24,25 +24,25 @@ movileNavToggle.addEventListener('click', () => {
 const navTabs = document.querySelectorAll('[role="tab"]');
 const articulo = document.querySelector('#articulo')
 const articuloImg = document.querySelector('#articulo--img')
-let contenido = '' 
+let contenido = ''
 
-navTabs.forEach( tab => {
- tab.addEventListener('click', cambiarTab)
+navTabs.forEach(tab => {
+  tab.addEventListener('click', cambiarTab)
 })
 
 async function obtenerData() {
-    const respuesta = await fetch('./data.json')
-    const data = await respuesta.json()
-    contenido = data
+  const respuesta = await fetch('./data.json')
+  const data = await respuesta.json()
+  contenido = data
 }
 obtenerData()
 
 function cambiarTab(e) {
   const selectedTab = e.target // Guardamos target tab 
-  
+
   const padreSeletedTab = selectedTab.parentNode // Buscamos el elemento padre de target tab seleccionado
 
-  padreSeletedTab.querySelector('[aria-selected="true"]').setAttribute('aria-selected', false ) // Entre los hijos de este elemento buscar uno con aria true y cambiar por false para quitar lo subrayado
+  padreSeletedTab.querySelector('[aria-selected="true"]').setAttribute('aria-selected', false) // Entre los hijos de este elemento buscar uno con aria true y cambiar por false para quitar lo subrayado
 
   selectedTab.setAttribute('aria-selected', true) // Agregamos subrayado al target tab 
 
@@ -63,7 +63,7 @@ function cambiarTab(e) {
 
   articulo.classList.add('opacidad-off')
   articuloImg.classList.add('opacidad-off')
-  
+
   function cambiarDestination() {
     articulo.innerHTML = `
     <h2 class="ff-serif fs-800 uppercase color-blanco">${contenidoSeleccionado.name}</h2>
@@ -79,7 +79,7 @@ function cambiarTab(e) {
     </div>
     </div>
     `
-    
+
     articuloImg.innerHTML = `
     <source srcset="${contenidoSeleccionado.images.webp}" type="image/webp">
     <img src="${contenidoSeleccionado.images.png}" alt="${contenidoSeleccionado.name}">
@@ -96,7 +96,7 @@ function cambiarTab(e) {
   </header>
   <p>${contenidoSeleccionado.bio}</p>
     `
-    
+
     articuloImg.innerHTML = `
     <source srcset="${contenidoSeleccionado.images.webp}" type="image/webp">
     <img src="${contenidoSeleccionado.images.png}" alt="${contenidoSeleccionado.name}">
@@ -106,42 +106,26 @@ function cambiarTab(e) {
   }
 
   function cambiarTechnology() {
+
     articulo.innerHTML = `
-    <h2 class="ff-serif fs-800 uppercase color-blanco">${contenidoSeleccionado.name}</h2>
-    <p>${contenidoSeleccionado.description}</p>
-    <div class="destination-meta flex">
-    <div>
-    <h3 class="color-claro fs-200 uppercase">ave.distance</h3>
-    <p class="ff-serif uppercase">${contenidoSeleccionado.distance}</p>
-    </div>
-    <div>
-    <h3 class="color-claro fs-200 uppercase">est.travel time</h3>
-    <p class="ff-serif uppercase">${contenidoSeleccionado.travel}</p>
-    </div>
-    </div>
+      <header class="flow flow--space-small">
+      <h2 class="fs-600 ff-serif uppercase">The terminology ...</h2>
+      <p class="fs-700 uppercase ff-serif color-blanco">${contenidoSeleccionado.name}</p>
+      </header>
+      <p>${contenidoSeleccionado.description}</p>
     `
-    
     articuloImg.innerHTML = `
-    <source srcset="${contenidoSeleccionado.images.webp}" type="image/webp">
-    <img src="${contenidoSeleccionado.images.png}" alt="${contenidoSeleccionado.name}">
+      <source
+        media="(orientation: landscape)"
+        srcset="${contenidoSeleccionado.images.portrait}" type="image/jpeg">
+      <source
+        media="(orientation: portrait)"
+        srcset="${contenidoSeleccionado.images.landscape}" type="image/jpeg">
+      <img src="${contenidoSeleccionado.images.landscape}" alt="Launch Vehicle">
     `
     articulo.classList.remove('opacidad-off')
     articuloImg.classList.remove('opacidad-off')
   }
-    
+
 }
 
-
-  /* Seudo codigo 
-
-   1- Amarrarse a todos los DOM
-   2- hacer un forEach a todos las pestañas que estan asigandos a las const navTabs con un eventlistener para poder hacerles con un click y lo hacemos desde la funcion cambiarTab
-   3- Dentro de la funcion con el evento target lo guardamos en la variable selectedTab
-  4- A esta variable con el metodo parenTNome buscamos el elemento padre y se lo asignamos a la variable padreSelectTab
-  5- A esta variable con el query selecionamos la clase aria que esta en true y con el metodo setattribute lo cambiamos por false para quitar lo subrayado 
-  6- Con este mismo metodo le agregamos el subrayado al targetTab
-  7- Desde la pestaña padre con el metodo getattribute inyectamos  la clase data-page y hacemos lo mismo con sus hijos para acceder a sus indices  y le asignamos este valor a una nueva variable para su mejor acceso
-  8- En la funcion con comillas invertidas (metodo spred operator ) mediante un id y el metodo innerhtml copiampos el contenido del articulo  y con el signo de peso cambiamos todo su contendio por estas variables y de esta manera se puede ver en el DOM
-  9- Hacemos lo mismo con la imagen
-  10- Con el metodo classlist add y remov e inyectando desde css opicity y transition conseguir una mejor animation del contenido y con la funcion seTimeeot controlamos el tiempo.
-  */
